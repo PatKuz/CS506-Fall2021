ad48eb64ffed53e13114eb612e97421d66a84cd3
PatKuz <>
2021-09-30 10:01:27 -0400

    Completed read.py, had to make modifications to use pytest directly because tox was not working.

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,12 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    matrix = []
+    with open(csv_file_path) as f:
+        for line in f:
+            x = line.replace('\n','').replace('\"','').split(',')
+            x = [int(i) if i.isdigit() else i for i in x]
+            matrix.append(x)
+    return matrix
+
+print(read_csv('test_files/dataset_2.csv'))
